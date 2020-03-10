51e527a0bc599873d61d1770c49ece6e814cd4e6
Auz <austinleehoward@gmail.com>
2020-03-10 08:32:17 -0500

    removed extra doc

--- future_docs/future_form.py
+++ future_docs/future_form.py
@@ -25,3 +25,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+#---------------------------------------------------------------
+
+class RegistrationForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    action = StringField('Action', validators=[DataRequired(), Length(min=2, max=20)])
+    element_type = StringField('Element Type', validators=[DataRequired()])
+    element_id = StringField('Element ID', validators=[DataRequired()])
+    input_test = StringField('Element ID', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That Username is taken. Please choose another one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose another one.')
